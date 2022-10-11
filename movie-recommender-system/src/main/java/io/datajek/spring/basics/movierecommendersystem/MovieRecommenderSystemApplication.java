1578375aa99d0400d5a8f2cd98cc1aa2b77a5b03
!Mensch <assassinste@gmail.com>
2022-10-11 10:49:15 +0800

    dev: Remove notes which is not in sync with the current resolution

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,6 +1,5 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
@@ -12,15 +11,6 @@ public class MovieRecommenderSystemApplication {
 		// Overview of Recommender
 		// -> it calls .recommendMovies
 		// ->     then .recommendMovies bases on types/names(<- filter)
-
-		// [When not using the 'Filter' interface]
-		// Currently we are "recommending" based on the 'Content' Filter, and
-		// it's already hardcoded in the implementation filter, every time we
-		// want to change it to other filters (e.g. like based on 'Genre'), we
-		// have to edit it again. But what we want to achieve is that change the
-		// tweak level to the top (i.e. here), so it's more like changing an
-		// method parameter, instead of changing the underlying code.
-		// RecommenderImplementation recommender = new RecommenderImplementation();
 
 		RecommenderImplementation recommender = new RecommenderImplementation(
 				new ContentBasedFilter()
