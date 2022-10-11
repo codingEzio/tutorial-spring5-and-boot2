7fb09a04af1a4e000c3c3f968cdf1e0a2735a9a7
!Mensch <assassinste@gmail.com>
2022-10-12 07:55:58 +0800

    dev: Initialize the 6th project to build on

---
+++ movie-recommender-system-06-springcore/src/main/java/io/datajek/spring/basics/movierecommendersystem06springcore/MovieRecommenderSystem06SpringcoreApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem06springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem06SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem06SpringcoreApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-06-springcore/src/test/java/io/datajek/spring/basics/movierecommendersystem06springcore/MovieRecommenderSystem06SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem06springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem06SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
