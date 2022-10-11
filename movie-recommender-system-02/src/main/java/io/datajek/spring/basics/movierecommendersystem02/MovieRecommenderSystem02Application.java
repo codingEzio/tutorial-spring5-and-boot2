7f57c5cac054fbb37e29dc359bcdd994ed76b3a6
!Mensch <assassinste@gmail.com>
2022-10-11 14:23:56 +0800

    dev: Initialize the 2nd project to build on

---
+++ movie-recommender-system-02/src/main/java/io/datajek/spring/basics/movierecommendersystem02/MovieRecommenderSystem02Application.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem02Application.class, args);
+	}
+
+}
---
+++ movie-recommender-system-02/src/test/java/io/datajek/spring/basics/movierecommendersystem02/MovieRecommenderSystem02ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
