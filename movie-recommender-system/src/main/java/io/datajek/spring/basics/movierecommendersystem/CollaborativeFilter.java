6652122ac27403f731ded50d83a172cccac20a0e
!Mensch <assassinste@gmail.com>
2022-10-11 10:57:12 +0800

    feat: Let IoC container to find and configure the objects automatically

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
@@ -1,8 +1,10 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CollaborativeFilter implements Filter {
 	public String[] getRecommendations(String movies) {
 		return new String[] {"Interstellar", "Matrix", "Blade Runner 2042"};
--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,9 +1,22 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 
+// It is equivalent to three annotations
+//  1. @ComponentScan
+//      which is a part of @SpringBootApplication, tells Spring where to find
+//      the objects which are annotated by @Component.
+//  2. @Configuration
+//      the source class for bean definitions (aka. the class who has an object
+//      who is about to be managed by the Spring container). This is quite like
+//      the relationship between (Component <> Autowired) and (@this <> @Bean).
+//  3. @EnableAutoConfiguration
+//      Enable auto-configuration of 'Spring Application Context', and attempt
+//      to guess and configure beans you're likely to need
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
@@ -12,8 +25,13 @@ public class MovieRecommenderSystemApplication {
 		// -> it calls .recommendMovies
 		// ->     then .recommendMovies bases on types/names(<- filter)
 
-		RecommenderImplementation recommender = new RecommenderImplementation(
-				new ContentBasedFilter()
+		ApplicationContext appContext = SpringApplication.run(
+				MovieRecommenderSystemApplication.class,
+				args
+		);
+
+		RecommenderImplementation recommender = appContext.getBean(
+				RecommenderImplementation.class
 		);
 
 		String[] results = recommender.recommendMovies("n/a");
