a224ac157405dff83a46b34b8dc564d25e947d92
!Mensch <assassinste@gmail.com>
2022-10-11 22:57:01 +0800

    dev: Initialize the 4th project to build on

---
+++ movie-recommender-system-04-lifecycle/src/main/java/io/datajek/spring/basics/movierecommendersystem04lifecycle/MovieRecommenderSystem04LifecycleApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem04lifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem04LifecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem04LifecycleApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-04-lifecycle/src/test/java/io/datajek/spring/basics/movierecommendersystem04lifecycle/MovieRecommenderSystem04LifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem04lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem04LifecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
