6d86bb8d460efda7582eef417f59e45f9c08afa3
!Mensch <assassinste@gmail.com>
2022-10-11 15:53:57 +0800

    dev: Initialize the 3rd project to build on

---
+++ movie-recommender-system-03/src/main/java/io/datajek/spring/basics/movierecommendersystem03/MovieRecommenderSystem03Application.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem03Application.class, args);
+	}
+
+}
---
+++ movie-recommender-system-03/src/test/java/io/datajek/spring/basics/movierecommendersystem03/MovieRecommenderSystem03ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
