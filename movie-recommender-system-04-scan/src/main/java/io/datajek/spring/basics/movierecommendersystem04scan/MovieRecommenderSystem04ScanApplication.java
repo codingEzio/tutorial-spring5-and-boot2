a0833e7c3121c10db228755725468249ec606ae2
!Mensch <assassinste@gmail.com>
2022-10-11 18:27:15 +0800

    dev: Initialize the 4th project to build on

---
+++ movie-recommender-system-04-scan/src/main/java/io/datajek/spring/basics/movierecommendersystem04scan/MovieRecommenderSystem04ScanApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem04scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem04ScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem04ScanApplication.class, args);
+	}
+
+}
