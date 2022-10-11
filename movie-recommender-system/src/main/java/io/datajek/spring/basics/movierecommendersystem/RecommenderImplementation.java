a1b9df38649749aa7257c5fb3df64e811b9b6053
!Mensch <assassinste@gmail.com>
2022-10-11 11:05:58 +0800

    dev: Remove the constructor as we no longer choose filter by params

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
@@ -13,14 +13,6 @@ public class RecommenderImplementation {
 	@Autowired
 	private Filter filter;
 
-	// Now you can choose (passing in) different filters when calling this
-	// implementation (in the main application, aka. the top level).
-	public RecommenderImplementation(Filter filter) {
-		super();
-
-		this.filter = filter;
-	}
-
 	public String[] recommendMovies(String movie) {
 		System.out.println("Current filter being used: [ " + filter + " ]");
 
