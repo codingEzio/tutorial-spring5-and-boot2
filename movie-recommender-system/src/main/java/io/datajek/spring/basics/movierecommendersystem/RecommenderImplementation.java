987e7a06c13dadfa65340bae7efd036c4dd0762b
!Mensch <assassinste@gmail.com>
2022-10-11 13:22:39 +0800

    dev: Notes on 'field injection' (good and bad)

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
@@ -7,6 +7,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class RecommenderImplementation {
 
+	// This is called 'Field Injection' (injected by @Qualifier in order to
+	// choose an appropriate bean), simple in the first sight, though it
+	// would be more complicated later on. Here's why:
+	//  1. Testing became more complex as we're doing IoC for fields right now
+	//  2. The amount of code and effort would become huge if your application
+	//     grows more complicated, basically it means if there were a lot of
+	//     fields other than the one for choosing filter, the constructor would
+	//     become quite complex and unmaintainable.
 	@Autowired
 	@Qualifier("CF")
 	private Filter filter;
