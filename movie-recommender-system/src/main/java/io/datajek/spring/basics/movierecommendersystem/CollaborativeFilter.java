d87a4cc1f0e7b640b438fa5b528dd7163dac8088
!Mensch <assassinste@gmail.com>
2022-10-11 10:53:14 +0800

    dev: Mark these filters for Spring (IoC) containers to manage

--- movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
@@ -1,5 +1,8 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CollaborativeFilter implements Filter {
 	public String[] getRecommendations(String movies) {
 		return new String[] {"Interstellar", "Matrix", "Blade Runner 2042"};
