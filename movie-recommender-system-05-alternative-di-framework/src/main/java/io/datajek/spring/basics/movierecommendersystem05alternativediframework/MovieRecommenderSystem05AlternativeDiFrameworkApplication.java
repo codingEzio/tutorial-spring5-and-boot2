fff11dbff15d9308a38697d382646bafa55ef9d4
!Mensch <assassinste@gmail.com>
2022-10-12 07:34:55 +0800

    dev: Initialize the 5th project to build on

---
+++ movie-recommender-system-05-alternative-di-framework/src/main/java/io/datajek/spring/basics/movierecommendersystem05alternativediframework/MovieRecommenderSystem05AlternativeDiFrameworkApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem05alternativediframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem05AlternativeDiFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem05AlternativeDiFrameworkApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-05-alternative-di-framework/src/test/java/io/datajek/spring/basics/movierecommendersystem05alternativediframework/MovieRecommenderSystem05AlternativeDiFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem05alternativediframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem05AlternativeDiFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
