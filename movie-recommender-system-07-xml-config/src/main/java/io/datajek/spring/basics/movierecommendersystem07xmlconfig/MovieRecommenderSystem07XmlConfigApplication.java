eb6cb447d65fd3fd0318561da6cdcd1216f5651a
!Mensch <assassinste@gmail.com>
2022-10-12 08:18:06 +0800

    dev: Initialize the 7th project to build on

---
+++ movie-recommender-system-07-xml-config/src/main/java/io/datajek/spring/basics/movierecommendersystem07xmlconfig/MovieRecommenderSystem07XmlConfigApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem07xmlconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem07XmlConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem07XmlConfigApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-07-xml-config/src/test/java/io/datajek/spring/basics/movierecommendersystem07xmlconfig/MovieRecommenderSystem07XmlConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem07xmlconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem07XmlConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
