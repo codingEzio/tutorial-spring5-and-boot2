05b361a0bcc02e254f7cbf50977d55369d7f8bc6
!Mensch <assassinste@gmail.com>
2022-10-12 08:35:30 +0800

    dev: XML way to configure the beans

--- movie-recommender-system-07-xml-config/src/main/java/io/datajek/spring/basics/movierecommendersystem07xmlconfig/MovieRecommenderSystem07XmlConfigApplication.java
+++ movie-recommender-system-07-xml-config/src/main/java/io/datajek/spring/basics/movierecommendersystem07xmlconfig/MovieRecommenderSystem07XmlConfigApplication.java
@@ -1,8 +1,18 @@
 package io.datajek.spring.basics.movierecommendersystem07xmlconfig;
 
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MovieRecommenderSystem07XmlConfigApplication {
 
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+
+		System.out.println("\nBeans loaded via XML");
+		System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()) + "\n");
+
+		appContext.close();
 	}
 
 }
