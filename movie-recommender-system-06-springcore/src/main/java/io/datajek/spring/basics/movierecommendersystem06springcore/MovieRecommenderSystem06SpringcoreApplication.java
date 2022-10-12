53f397700b2af484fdc6415a6e5495127e84a714
!Mensch <assassinste@gmail.com>
2022-10-12 08:12:53 +0800

    feat: Steps to switch from Spring Boot to Spring Core
    Basically, we need to do these things. One, importing dependencies from
    the core library so we could get the application started; Second, change
    the "smart" annotation to explicit annotations underneath; Third, add
    the code for closing the application as when we're using Spring Boot, it
    would do them for us.

--- movie-recommender-system-06-springcore/src/main/java/io/datajek/spring/basics/movierecommendersystem06springcore/MovieRecommenderSystem06SpringcoreApplication.java
+++ movie-recommender-system-06-springcore/src/main/java/io/datajek/spring/basics/movierecommendersystem06springcore/MovieRecommenderSystem06SpringcoreApplication.java
@@ -1,13 +1,26 @@
 package io.datajek.spring.basics.movierecommendersystem06springcore;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
-@SpringBootApplication
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// @SpringBootApplication
+@Configuration
+@ComponentScan
 public class MovieRecommenderSystem06SpringcoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieRecommenderSystem06SpringcoreApplication.class, args);
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(
+				MovieRecommenderSystem06SpringcoreApplication.class);
+
+		RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
+
+		String[] result = recommender.recommendMovies("n/a");
+		System.out.println(Arrays.toString(result));
+
+		appContext.close();
 	}
 
 }
