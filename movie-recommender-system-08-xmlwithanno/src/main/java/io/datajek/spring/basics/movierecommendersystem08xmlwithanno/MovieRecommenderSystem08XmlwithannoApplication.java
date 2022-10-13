2d88eec9aa8858f786e4f53bfc81b38d4f0eb160
!Mensch <assassinste@gmail.com>
2022-10-13 10:42:14 +0800

    dev: Initialize the 8th project to build on

---
+++ movie-recommender-system-08-xmlwithanno/src/main/java/io/datajek/spring/basics/movierecommendersystem08xmlwithanno/MovieRecommenderSystem08XmlwithannoApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem08xmlwithanno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem08XmlwithannoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem08XmlwithannoApplication.class, args);
+	}
+
+}
