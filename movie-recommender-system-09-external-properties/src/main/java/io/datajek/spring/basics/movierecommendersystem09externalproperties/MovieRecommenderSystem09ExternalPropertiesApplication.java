e14a6b4f871255c88b90cb340f2a424518bb08fb
!Mensch <assassinste@gmail.com>
2022-10-19 06:34:12 +0800

    dev: Initialize the 9th project to build on

---
+++ movie-recommender-system-09-external-properties/src/main/java/io/datajek/spring/basics/movierecommendersystem09externalproperties/MovieRecommenderSystem09ExternalPropertiesApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem09externalproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem09ExternalPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem09ExternalPropertiesApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-09-external-properties/src/test/java/io/datajek/spring/basics/movierecommendersystem09externalproperties/MovieRecommenderSystem09ExternalPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem09externalproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem09ExternalPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
