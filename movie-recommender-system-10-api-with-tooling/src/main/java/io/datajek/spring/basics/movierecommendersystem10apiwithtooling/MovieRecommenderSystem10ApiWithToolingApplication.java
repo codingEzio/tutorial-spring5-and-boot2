ee0418cd39181da46ec99dfb2543b07d7f40b55f
!Mensch <assassinste@gmail.com>
2022-10-19 08:13:10 +0800

    dev: Initialize the 10th project to build on

---
+++ movie-recommender-system-10-api-with-tooling/src/main/java/io/datajek/spring/basics/movierecommendersystem10apiwithtooling/MovieRecommenderSystem10ApiWithToolingApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem10apiwithtooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem10ApiWithToolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem10ApiWithToolingApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-10-api-with-tooling/src/test/java/io/datajek/spring/basics/movierecommendersystem10apiwithtooling/MovieRecommenderSystem10ApiWithToolingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem10apiwithtooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem10ApiWithToolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
