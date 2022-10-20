7ff576ae4872f16f225adc69d03687d4bb02c441
!Mensch <assassinste@gmail.com>
2022-10-21 07:07:15 +0800

    dev: Initialize the 11th project to build on

---
+++ movie-recommender-system-11-jdbc/src/main/java/io/datajek/spring/basics/movierecommendersystem11jdbc/MovieRecommenderSystem11JdbcApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem11jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystem11JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderSystem11JdbcApplication.class, args);
+	}
+
+}
---
+++ movie-recommender-system-11-jdbc/src/test/java/io/datajek/spring/basics/movierecommendersystem11jdbc/MovieRecommenderSystem11JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem11jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystem11JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
