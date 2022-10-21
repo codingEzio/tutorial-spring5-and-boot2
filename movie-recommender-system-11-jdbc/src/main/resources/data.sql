ea3b9de7c93a7a529c2606af45a6dc274a248b7e
!Mensch <assassinste@gmail.com>
2022-10-21 08:20:12 +0800

    dev: Database prep and test data preload

---
+++ movie-recommender-system-11-jdbc/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (1, 'Jake', 'Congo', '1993-12-02', 63);
+
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (2, 'Oliver', 'Serbia', '1987-05-22', 81);
+
+INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
+VALUES (3, 'Tammy', 'France', '1986-09-01', 10);
