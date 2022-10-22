6f805d5a6f02c3a5c70b030d5925086714bddf7b
!Mensch <assassinste@gmail.com>
2022-10-22 08:08:22 +0800

    dev: Tiny tweaks to the test data

--- movie-recommender-system-11-jdbc/src/main/resources/data.sql
+++ movie-recommender-system-11-jdbc/src/main/resources/data.sql
@@ -1,8 +1,11 @@
 INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
-VALUES (1, 'Jake', 'Congo', '1993-12-02', 63);
+VALUES
+    (1, 'Jake', 'Scotland', '1993-12-02', 63);
 
 INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
-VALUES (2, 'Oliver', 'Serbia', '1987-05-22', 81);
+VALUES
+    (2, 'Oliver', 'Ireland', '1987-05-22', 81);
 
 INSERT INTO Player (ID, Name, Nationality, Birth_date, Titles)
-VALUES (3, 'Tammy', 'France', '1986-09-01', 10);
+VALUES
+    (3, 'Tammy', 'Isle of Man', '1986-09-01', 10);
