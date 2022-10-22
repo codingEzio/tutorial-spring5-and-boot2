97b8c4397827bdda7d05e6bedbe50f5b99787eaf
!Mensch <assassinste@gmail.com>
2022-10-22 16:03:16 +0800

    feat: Implement C(R)UD for REST service

--- rest-01/src/main/java/io/datajek/spring/basics/rest01/PlayerController.java
+++ rest-01/src/main/java/io/datajek/spring/basics/rest01/PlayerController.java
@@ -1,13 +1,30 @@
 package io.datajek.spring.basics.rest01;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
 
+    @Autowired
+    PlayerService playerService;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "This is Houston.";
+    }
+
+    @GetMapping("/players")
+    public List<Player> allPlayers() {
+        return playerService.getAllPlayers();
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayer(@PathVariable int id) {
+        return playerService.getPlayer(id);
     }
 }
--- rest-01/src/main/java/io/datajek/spring/basics/rest01/PlayerService.java
+++ rest-01/src/main/java/io/datajek/spring/basics/rest01/PlayerService.java
@@ -1,5 +1,8 @@
 package io.datajek.spring.basics.rest01;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,19 @@ public class PlayerService {
 
     @Autowired
     private PlayerRepository playerRepository;
+
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Player getPlayer(int id) {
+        Optional<Player> player = playerRepository.findById(id);
+        Player p = null;
+
+        if (player.isPresent()) {
+            p = player.get();
+        }
+
+        return p;
+    }
 }
