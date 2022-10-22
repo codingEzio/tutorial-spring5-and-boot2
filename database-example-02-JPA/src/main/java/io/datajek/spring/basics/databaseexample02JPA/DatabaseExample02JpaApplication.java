c3dc8cdbd361d5223aba6161274c678f6e2dc6c9
!Mensch <assassinste@gmail.com>
2022-10-22 08:46:24 +0800

    dev: Initialize the 12th project to build on

---
+++ database-example-02-JPA/src/main/java/io/datajek/spring/basics/databaseexample02JPA/DatabaseExample02JpaApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.databaseexample02JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseExample02JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseExample02JpaApplication.class, args);
+	}
+
+}
---
+++ database-example-02-JPA/src/test/java/io/datajek/spring/basics/databaseexample02JPA/DatabaseExample02JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.databaseexample02JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseExample02JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
