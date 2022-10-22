4fc9351ec2ac64cdf8cfd2abf07a318fe15c6b71
!Mensch <assassinste@gmail.com>
2022-10-22 12:49:29 +0800

    feat: Transactional JPA to implement CRUD for entity 'Player'

---
+++ database-example-02-JPA/src/main/java/io/datajek/spring/basics/databaseexample02JPA/PlayerRepository.java
@@ -0,0 +1,33 @@
+package io.datajek.spring.basics.databaseexample02JPA;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
+public class PlayerRepository {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	public Player insertPlayer(Player player) {
+		return entityManager.merge(player);
+	}
+
+	public Player updatePlayer(Player player) {
+		return entityManager.merge(player);
+	}
+
+	public Player getPlayerById(int id) {
+		return entityManager.find(Player.class, id);
+	}
+
+	public void deleteById(int id ) {
+		Player player = entityManager.find(Player.class, id);
+
+		entityManager.remove(player);
+	}
+}
