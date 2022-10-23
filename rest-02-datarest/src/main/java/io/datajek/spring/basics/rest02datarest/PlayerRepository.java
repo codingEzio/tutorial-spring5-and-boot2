6dba9506d87f0892889c11d7c19d63483c19b3cb
!Mensch <assassinste@gmail.com>
2022-10-23 11:43:22 +0800

    dev: Tweak API path

--- rest-02-datarest/src/main/java/io/datajek/spring/basics/rest02datarest/PlayerRepository.java
+++ rest-02-datarest/src/main/java/io/datajek/spring/basics/rest02datarest/PlayerRepository.java
@@ -1,6 +1,8 @@
 package io.datajek.spring.basics.rest02datarest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "athletes")
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 }
