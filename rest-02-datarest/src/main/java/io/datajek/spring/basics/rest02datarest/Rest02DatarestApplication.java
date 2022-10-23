f477cfcfaa3a4258849430f3b87e5fcd425ad8ae
!Mensch <assassinste@gmail.com>
2022-10-23 11:05:19 +0800

    dev: Initialize the 14th project to build on

---
+++ rest-02-datarest/src/main/java/io/datajek/spring/basics/rest02datarest/Rest02DatarestApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.rest02datarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest02DatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest02DatarestApplication.class, args);
+	}
+
+}
---
+++ rest-02-datarest/src/test/java/io/datajek/spring/basics/rest02datarest/Rest02DatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.rest02datarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest02DatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
