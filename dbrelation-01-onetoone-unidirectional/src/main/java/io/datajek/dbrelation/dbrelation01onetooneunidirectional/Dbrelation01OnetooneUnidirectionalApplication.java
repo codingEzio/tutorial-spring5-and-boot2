85fac70e3a7a0d193ba9ffed94a0a84e80242951
!Mensch <assassinste@gmail.com>
2022-11-04 21:47:30 +0800

    dev: Initialize the 15th project to build on

---
+++ dbrelation-01-onetoone-unidirectional/src/main/java/io/datajek/dbrelation/dbrelation01onetooneunidirectional/Dbrelation01OnetooneUnidirectionalApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.dbrelation.dbrelation01onetooneunidirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbrelation01OnetooneUnidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbrelation01OnetooneUnidirectionalApplication.class, args);
+	}
+
+}
---
+++ dbrelation-01-onetoone-unidirectional/src/test/java/io/datajek/dbrelation/dbrelation01onetooneunidirectional/Dbrelation01OnetooneUnidirectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.dbrelation.dbrelation01onetooneunidirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbrelation01OnetooneUnidirectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
